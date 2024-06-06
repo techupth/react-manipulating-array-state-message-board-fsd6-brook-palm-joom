aeed9c26c2288298c20403a078112c9d9df83015
Joom <jnwongsa@gmail.com>
2024-06-06 14:19:44 +0700

    update:ex array-state/Joom

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,23 +1,57 @@
+import { useState } from "react";
+
 function MessageBoard() {
+  const [messageInput, setMessageInput] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newMessage = [...messageList];
+    newMessage.push(messageInput);
+    setMessageList(newMessage);
+    setMessageInput("");
+  };
+
+  const handleRemove = (index) => {
+    const newMessage = [...messageList];
+    newMessage.splice(index, 1);
+    setMessageList(newMessage);
+  };
+
   return (
     <div className="app-wrapper">
       <h1 class="app-title">Message board</h1>
-      <div class="message-input-container">
+      <form class="message-input-container" onSubmit={handleSubmit}>
         <label>
           <input
             id="message-text"
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            value={messageInput}
+            onChange={(event) => {
+              setMessageInput(event.target.value);
+            }}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
-      </div>
-      <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+        <button className="submit-message-button" type="submit">Submit</button>
+      </form>
+      <div className="board">
+        {messageList.map((item, index) => {
+          return (
+            <div className="message" key={index}>
+              <h1>{item}</h1>
+              <button
+                className="delete-button"
+                onClick={() => {
+                  handleRemove(index);
+                }}
+              >
+                x
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
